695c363c2f171ced834b64a192778c9a0c92fbb2
pongo1231 <pongo1999712@gmail.com>
2020-12-09 21:00:57 +0100

    Changes to Labels Randomizer

--- RandomizerV/Randomizers/Labels.cpp
+++ RandomizerV/Randomizers/Labels.cpp
@@ -1,18 +1,18 @@
 #include <stdafx.h>
 
-static std::unordered_map<Hash, Hash> m_labelsMap;
+static std::unordered_map<Hash, Hash> m_labelHashMap;
 
-static __int64 m_labelPool = 0;
+static __int64 m_labelTable = 0;
 
 static inline Hash GetReplacementLabelHash(Hash _labelHash)
 {
-	if (m_labelsMap.empty())
+	if (m_labelHashMap.empty())
 	{
 		return _labelHash;
 	}
 
-	const auto& result = m_labelsMap.find(_labelHash);
-	if (result != m_labelsMap.end())
+	const auto& result = m_labelHashMap.find(_labelHash);
+	if (result != m_labelHashMap.end())
 	{
 		return result->second;
 	}
@@ -22,7 +22,7 @@ static inline Hash GetReplacementLabelHash(Hash _labelHash)
 
 static inline const char* GetLabelText(Hash _labelHash)
 {
-	if (!m_labelPool)
+	if (!m_labelTable)
 	{
 		return "NULL";
 	}
@@ -40,13 +40,13 @@ static inline const char* GetLabelText(Hash _labelHash)
 		sub_7FF79F04C244 = handle.Get<const char*(__int64, Hash)>();
 	}
 
-	return sub_7FF79F04C244(m_labelPool, _labelHash);
+	return sub_7FF79F04C244(m_labelTable, _labelHash);
 }
 
 const char* (*_GET_LABEL_TEXT_og)(const char* _label);
 const char* _GET_LABEL_TEXT_hook(const char* _label)
 {
-	return GetLabelText(GetReplacementLabelHash(GET_HASH_KEY(_label)));
+	return GetLabelText(GetReplacementLabelHash(_GET_HASH_KEY(_label)));
 
 	//return _GET_LABEL_TEXT_og(_label);
 }
@@ -54,7 +54,7 @@ const char* _GET_LABEL_TEXT_hook(const char* _label)
 const char*(*sub_7FF79F04C28C_og)(__int64 _a1, const char* _label);
 const char* __fastcall sub_7FF79F04C28C_hook(__int64 _a1, const char* _label)
 {
-	return GetLabelText(GetReplacementLabelHash(GET_HASH_KEY(_label)));
+	return GetLabelText(GetReplacementLabelHash(_GET_HASH_KEY(_label)));
 
 	//return sub_7FF79F04C28C_og(_a1, _label);
 }
@@ -63,6 +63,12 @@ const char*(*sub_7FF79E422238_og)(const char* _label, DWORD64* _a2, __int64 _a3)
 const char* sub_7FF79E422238_hook(const char* _label, DWORD64* _a2, __int64 _a3)
 {
 	return "NULL";
+}
+
+const char*(*sub_7FF79F05142C_og)(__int64 _a1, Hash _labelHash, __int64 _a3);
+const char* sub_7FF79F05142C_hook(__int64 _a1, Hash _labelHash, __int64 _a3)
+{
+	return sub_7FF79F05142C_og(_a1, GetReplacementLabelHash(_labelHash), _a3);
 }
 
 static void OnHook()
@@ -77,21 +83,26 @@ static void OnHook()
 	//
 
 	handle = Memory::FindPattern("40 53 48 83 EC 20 48 8B D9 48 8B D1 48 8D 0D ? ? ? ? E8 ? ? ? ? 84 C0 74 34 48 8D 0D ? ? ? ? 48 8B D3");
-	Memory::AddHook("_GET_LABEL_TEXT", handle.Get<void>(), _GET_LABEL_TEXT_hook, &_GET_LABEL_TEXT_og);
+	//Memory::AddHook("_GET_LABEL_TEXT", handle.Get<void>(), _GET_LABEL_TEXT_hook, &_GET_LABEL_TEXT_og);
 
 	//
 
-	m_labelPool = handle.At(0xC).At(2).Into().Addr();
+	m_labelTable = handle.At(0xC).At(2).Into().Addr();
 
 	//
 
 	handle = Memory::FindPattern("48 89 5C 24 ? 57 48 83 EC 20 48 8B DA 48 8B F9 48 85 D2 75 44 E8");
-	Memory::AddHook("sub_7FF79F04C28C", handle.Get<void>(), sub_7FF79F04C28C_hook, &sub_7FF79F04C28C_og);
+	//Memory::AddHook("sub_7FF79F04C28C", handle.Get<void>(), sub_7FF79F04C28C_hook, &sub_7FF79F04C28C_og);
 
 	//
 
-	/*handle = Memory::FindPattern("4C 8B DA 48 8B C1 41 B9 ? ? ? ? EB 18 4D 8B 13 4D 85 D2 74 15 43 8A 14 0A 84 D2 74 0D 88 11 48 FF C1 49 FF C1");
-	Memory::AddHook("sub_7FF79E422238", handle.Get<void>(), sub_7FF79E422238_hook, &sub_7FF79E422238_og);*/
+	//handle = Memory::FindPattern("4C 8B DA 48 8B C1 41 B9 ? ? ? ? EB 18 4D 8B 13 4D 85 D2 74 15 43 8A 14 0A 84 D2 74 0D 88 11 48 FF C1 49 FF C1");
+	//Memory::AddHook("sub_7FF79E422238", handle.Get<void>(), sub_7FF79E422238_hook, &sub_7FF79E422238_og);
+
+	//
+
+	handle = Memory::FindPattern("E8 ? ? ? ? 48 8B F8 48 85 C0 74 0C C7 83 ? ? ? ? ? ? ? ? EB 60 33 ED 33 F6");
+	Memory::AddHook("sub_7FF79F05142C", handle.Into().Get<void>(), sub_7FF79F05142C_hook, &sub_7FF79F05142C_og);
 }
 
 static void OnTick()
@@ -104,16 +115,16 @@ static void OnTick()
 	{
 		timestamp = curTimestamp;
 
-		Handle handle = m_labelPool;
+		const Handle handle = m_labelTable;
 
 		int thisLabelsCount = 0;
 
 		for (int i = 0; i < 20; i++)
 		{
-			Handle poolHandle = handle.At(8 * i).Value<__int64>();
-			unsigned int poolSize = poolHandle.At(4).Value<unsigned int>();
+			Handle rowHandle = handle.At(8 * i).Value<__int64>();
+			unsigned int rowSize = rowHandle.At(4).Value<unsigned int>();
 
-			thisLabelsCount += poolSize;
+			thisLabelsCount += rowSize;
 		}
 
 		if (labelsCount != thisLabelsCount)
@@ -122,20 +133,20 @@ static void OnTick()
 
 			labelsCount = thisLabelsCount;
 
-			m_labelsMap.clear();
-
 			std::vector<Hash> labelHashList;
 
 			for (int i = 0; i < 20; i++)
 			{
-				Handle poolHandle = handle.At(8 * i).Value<__int64>();
-				unsigned int poolSize = poolHandle.At(4).Value<unsigned int>();
+				Handle rowHandle = handle.At(8 * i).Value<__int64>();
+				unsigned int rowSize = rowHandle.At(4).Value<unsigned int>();
 
-				for (int j = 0; j < poolSize; j++)
+				for (int j = 0; j < rowSize; j++)
 				{
-					labelHashList.push_back(poolHandle.At(8 * j).Value<Hash>());
+					labelHashList.push_back(rowHandle.At(8 * j).Value<Hash>());
 				}
 			}
+
+			m_labelHashMap.clear();
 
 			std::vector<Hash> temp = labelHashList;
 
@@ -143,7 +154,7 @@ static void OnTick()
 			{
 				int chosenIndex = Random::GetRandomInt(0, temp.size() - 1);
 
-				m_labelsMap[labelHash] = temp[chosenIndex];
+				m_labelHashMap[labelHash] = temp[chosenIndex];
 
 				temp.erase(temp.begin() + chosenIndex);
 			}
